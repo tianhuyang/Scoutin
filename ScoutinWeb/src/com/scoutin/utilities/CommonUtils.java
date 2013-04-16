b40a9514d48c5f816b9962561896d87895878e04
tianhuyang@gmail.com <tianhuyang@gmail.com>
2013-04-15 22:15:33 -0700

    add describe method in CommonUtils

--- ScoutinWeb/src/com/scoutin/utilities/CommonUtils.java
+++ ScoutinWeb/src/com/scoutin/utilities/CommonUtils.java
@@ -1,7 +1,22 @@
 package com.scoutin.utilities;
 
+import java.lang.reflect.Field;
 import java.util.Map;
 
 public class CommonUtils {
 
+	public void describe(Map<String,Object> map, Object bean){
+		Field[] fields = bean.getClass().getDeclaredFields();
+		for(Field field : fields){
+			try {
+				map.put(field.getName(), field.get(bean));
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+			}
+		}
+	}
 }
